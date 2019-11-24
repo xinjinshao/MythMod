623453e081c54bb74ca257f1bfab9bc40861abd7
xinjinshao <394726202@qq.com>
2019-11-24 13:08:25 +0800

    add chrismas gift

---
+++ Items/ChrismasGift.cs
@@ -0,0 +1,32 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MythMode.Items
+{
+	public class ChrismasGift : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("ChrismasGift");
+			Tooltip.SetDefault("<right> for goodies!");
+		}
+
+		public override void SetDefaults() {
+			item.width = 20;
+			item.height = 20;
+			item.rare = 2;
+		}
+
+		public override bool CanRightClick() {
+			return true;
+		}
+
+		public override void RightClick(Player player) {
+            player.QuickSpawnItem(ItemID.CellPhone);
+            player.QuickSpawnItem(ItemID.FrostsparkBoots);
+            player.QuickSpawnItem(ItemID.CandyCaneSword);
+            player.QuickSpawnItem(ItemID.DogWhistle);
+        }
+	}
+}
